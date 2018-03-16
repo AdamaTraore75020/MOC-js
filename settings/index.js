1da1d1e87a92ae012dff7c6476b9db23f69aa533
Adama <a.traore.pro@gmail.com>
2018-03-16 10:56:24 +0100

    server config port minus

--- settings/index.js
+++ settings/index.js
@@ -1,7 +1,7 @@
 module.exports = server => {
     server.settings = require('./settings.json');
 
-    server.settings.PORT = process.env.PORT || server.settings.PORT;
+    server.settings.port = process.env.PORT || server.settings.port;
     server.settings.db_uri = process.env.MONGODB_URI || server.settings.db_uri;
 
 };
